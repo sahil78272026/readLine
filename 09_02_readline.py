a4ba8fe53f308186986cd73c89adcf3b60abe425
sahil78272026 <sahil.garg78272026@gmail.com>
2022-05-04 22:43:18 +0530

    Add files via upload

---
+++ 09_02_readline.py
@@ -0,0 +1,11 @@
+f = open("D:\\Python\\Python Course with Notes\\9. Chapter 9\\another.txt",'r') 
+data = f.readline() # Reads the line and add new line character in the end
+print(data)
+data = f.readline() # Reads the line and add new line character in the end
+print(data)
+data = f.readline() # Reads the line and add new line character in the end
+print(data)
+data = f.readline() # Reads the line and add new line character in the end
+print(data)
+
+f.close()
